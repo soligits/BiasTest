24f874547bdc234bc4799bbcf127ed8e46c0b1c3
Mohammad Jafari <mamad.jafari91@gmail.com>
2023-07-27 15:24:29 +0330

    add CUB dataset

--- MSAD/utils.py
+++ MSAD/utils.py
@@ -20,6 +20,11 @@ import requests
 import subprocess
 from robustness import model_utils
 from robustness.datasets import ImageNet
+import os
+import pandas as pd
+from torchvision.datasets import VisionDataset
+from torchvision.datasets.folder import default_loader
+from torchvision.datasets.utils import download_file_from_google_drive
 
 
 class GaussianBlur(object):
@@ -262,6 +267,8 @@ def get_train_dataset(dataset, label_class, path, backbone):
         return get_SVHN_train(label_class, path, backbone)
     elif dataset == "mvtec":
         return get_MVTEC_train(label_class, path, backbone)
+    elif dataset == "cub":
+        return get_CUB_train(label_class, path, backbone)
     else:
         raise Exception("Source Dataset is not supported yet. ")
         exit()
@@ -280,6 +287,8 @@ def get_test_dataset(dataset, normal_labels, path, backbone):
         return get_SVHN_test(normal_labels, path, backbone)
     elif dataset == "mvtec":
         return get_MVTEC_test(normal_labels, path, backbone)
+    elif dataset == "cub":
+        return get_CUB_test(normal_labels, path, backbone)
     else:
         raise Exception("Target Dataset is not supported yet. ")
         exit()
@@ -736,4 +745,200 @@ def get_MVTEC_test(normal_class_labels, path, backbone):
         all_testsets.append(testset)
 
     testset = ConcatDataset(all_testsets)
+    return testset
+
+
+class Cub2011(VisionDataset):
+    """`CUB-200-2011 <http://www.vision.caltech.edu/visipedia/CUB-200-2011.html>`_ Dataset.
+
+    Args:
+        root (string): Root directory of the dataset.
+        train (bool, optional): If True, creates dataset from training set, otherwise
+           creates from test set.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+           and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the
+           target and transforms it.
+        download (bool, optional): If true, downloads the dataset from the internet and
+           puts it in root directory. If dataset is already downloaded, it is not
+           downloaded again.
+    """
+
+    base_folder = "CUB_200_2011/images"
+    # url = 'http://www.vision.caltech.edu/visipedia-data/CUB-200-2011/CUB_200_2011.tgz'
+    file_id = "1hbzc_P1FuxMkcabkgn9ZKinBwW683j45"
+    filename = "CUB_200_2011.tgz"
+    tgz_md5 = "97eceeb196236b17998738112f37df78"
+
+    def __init__(
+        self, root, train=True, transform=None, target_transform=None, download=False
+    ):
+        super(Cub2011, self).__init__(
+            root, transform=transform, target_transform=target_transform
+        )
+
+        self.loader = default_loader
+        self.train = train
+        if download:
+            self._download()
+
+        if not self._check_integrity():
+            raise RuntimeError(
+                "Dataset not found or corrupted. You can use download=True to download it"
+            )
+
+    def _load_metadata(self):
+        images = pd.read_csv(
+            os.path.join(self.root, "CUB_200_2011", "images.txt"),
+            sep=" ",
+            names=["img_id", "filepath"],
+        )
+        image_class_labels = pd.read_csv(
+            os.path.join(self.root, "CUB_200_2011", "image_class_labels.txt"),
+            sep=" ",
+            names=["img_id", "target"],
+        )
+        train_test_split = pd.read_csv(
+            os.path.join(self.root, "CUB_200_2011", "train_test_split.txt"),
+            sep=" ",
+            names=["img_id", "is_training_img"],
+        )
+
+        data = images.merge(image_class_labels, on="img_id")
+        self.data = data.merge(train_test_split, on="img_id")
+
+        class_names = pd.read_csv(
+            os.path.join(self.root, "CUB_200_2011", "classes.txt"),
+            sep=" ",
+            names=["class_name"],
+            usecols=[1],
+        )
+        self.class_names = class_names["class_name"].to_list()
+        if self.train:
+            self.data = self.data[self.data.is_training_img == 1]
+        else:
+            self.data = self.data[self.data.is_training_img == 0]
+
+    def _check_integrity(self):
+        try:
+            self._load_metadata()
+        except Exception:
+            return False
+
+        for index, row in self.data.iterrows():
+            filepath = os.path.join(self.root, self.base_folder, row.filepath)
+            if not os.path.isfile(filepath):
+                print(filepath)
+                return False
+        return True
+
+    def _download(self):
+        import tarfile
+
+        if self._check_integrity():
+            print("Files already downloaded and verified")
+            return
+
+        download_file_from_google_drive(
+            self.file_id, self.root, self.filename, self.tgz_md5
+        )
+
+        with tarfile.open(os.path.join(self.root, self.filename), "r:gz") as tar:
+            tar.extractall(path=self.root)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        sample = self.data.iloc[idx]
+        path = os.path.join(self.root, self.base_folder, sample.filepath)
+        target = sample.target
+        img = self.loader(path)
+
+        if self.transform is not None:
+            img = self.transform(img)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        return img, target
+
+
+class CUBAnomaly(Cub2011):
+    def __init__(self, root, normal_classes, subclasses, **kwargs):
+        super().__init__(root, **kwargs)
+        self.normal_classes = normal_classes
+
+        if subclasses is None:
+            subclasses = list(range(200))
+
+        self.subclasses = subclasses
+
+        if self.train and not set(normal_classes).issubset(set(subclasses)):
+            raise ValueError("Normal classes must be a subset of subclasses")
+
+        classes = self.normal_classes if self.train else self.subclasses
+        self.subset_classes(classes)
+
+    def subset_classes(self, classes):
+        # Filter data
+        subclasses = classes
+        subset_data = self.data[self.data["target"].isin(subclasses)]
+        self.data = subset_data
+
+        # Update attributes
+        self.class_names = [self.class_names[i] for i in subclasses]
+
+    @property
+    def num_classes(self):
+        return len(self.class_names)
+
+    def __getitem__(self, idx):
+        img, target = super().__getitem__(idx)
+
+        if self.train:
+            if target in self.normal_classes:
+                label = 0
+        else:
+            if target in self.normal_classes:
+                label = 0
+            else:
+                label = 1
+
+        return img, label
+
+
+def get_CUB_train(normal_class_labels, path, backbone):
+    transform = transform_color if backbone == "152" else transform_resnet18_color
+
+    trainset = CUBAnomaly(
+        root=path,
+        train=True,
+        download=True,
+        normal_classes=normal_class_labels,
+        subclasses=list(range(20)),
+        transform=transform,
+    )
+    trainset_moco = CUBAnomaly(
+        root=path,
+        train=True,
+        download=True,
+        normal_classes=normal_class_labels,
+        subclasses=list(range(20)),
+        transform=Transform(),
+    )
+
+    return trainset, trainset_moco
+
+
+def get_CUB_test(normal_class_labels, path, backbone):
+    transform = transform_color if backbone == "152" else transform_resnet18_color
+
+    testset = CUBAnomaly(
+        root=path,
+        train=False,
+        download=True,
+        normal_classes=normal_class_labels,
+        subclasses=list(range(20)),
+        transform=transform,
+    )
+
     return testset
