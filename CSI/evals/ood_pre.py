0d1fb7cde2c5c474f907339fe57797685a341944
soligits <m.soltani.252@gmail.com>
2023-10-24 01:05:49 +0330

    update

--- CSI/evals/ood_pre.py
+++ CSI/evals/ood_pre.py
@@ -188,7 +188,7 @@ def _get_features(P, model, loader, interp=False, imagenet=False, simclr_aug=Non
                 x_t = torch.cat([P.shift_trans(hflip(x), k) for k in range(P.K_shift)])
             else:
                 x_t = x # No shifting: SimCLR
-            x_t = simclr_aug(x_t)
+            x_t = simclr_aug(x_t.to(device)).cpu()
 
             # compute augmented features
             with torch.no_grad():
